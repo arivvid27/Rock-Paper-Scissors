65ba2f53a708ba7e9114fc29d2f0512066acbae2
Anonymous <arivvid27@outlook.com>
2020-12-28 19:36:48 +0000

    I updated my Pypi publication files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rock-Paper-Scissors
+
+This is just a game that one can play if no one is around.
+
+# Controls
+
+[r]ock
+[p]aper
+[s]cissors
+
+# Bugs
+It may get stuck and say 'What?' a few times. if that happens, just keep rerunning the program until it outputs the correct result.
--- setup.py
+++ setup.py
@@ -9,5 +9,13 @@ setuptools.setup(
 	author_email = "arivvid27@outlook.com",
 	description = "A 'Rock Paper Scissors' game.",
 	long_description = long_description,
-
+	long_description_content_type="text/markdown",
+	url = "https://github.com/arivvid27/Rock-Paper-Scissors",
+	packages = setuptools.find_packages(),
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+	],
+	python_requires = '>=3.6',
 )
